f00d00c8f67ceae146dbc72a40b00e89a5e3b901
lexville <alekikangethe@gmail.com>
2018-08-29 20:46:02 +0300

    Add base template as well as main css

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		log.Fatalln("Failed to start the server")
